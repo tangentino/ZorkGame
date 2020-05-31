3a7a5241255f4b92674690c0ecb993912b8c7bd6
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2020-05-31 16:51:42 +0700

    Create Room.java

---
+++ zorkgame/src/main/java/io/muzoo/ooc/homework2/Room.java
@@ -0,0 +1,10 @@
+package io.muzoo.ooc.homework2;
+
+import io.muzoo.ooc.homework2.item.Item;
+
+import java.util.HashMap;
+
+public class Room {
+    private Monster monster;
+    private HashMap<String,Item> items;
+}
